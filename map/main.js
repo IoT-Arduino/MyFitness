1a51cb37441e6a48d841cfb0aa6f5c1e114e7563
unknown <bunkajin@icloud.com>
2020-04-29 17:54:22 +0900

    change comments and variable declaration

--- map/main.js
+++ map/main.js
@@ -1,8 +1,8 @@
 const currentWindow = null
 const maplist = new Array()
 let cnt = 0
-const maplistUl = document.getElementById('mapList')
-let maplistLi = document.querySelectorAll('#mapList li')
+let maplistUl = document.getElementById("mapList")
+let maplistLi = document.querySelectorAll("#mapList li")
 
 //　LocationのJSONデータの取得
 class GetData {
@@ -11,26 +11,32 @@ class GetData {
       let result = await fetch("location.json")
       let data = await result.json()
       let markers = data.items
-      
-      markers = markers.map(item => {
-        const { title, latitude, longitude,address,id,category } = item.fields
+
+      markers = markers.map((item) => {
+        const {
+          title,
+          latitude,
+          longitude,
+          address,
+          id,
+          category,
+        } = item.fields
         const image = item.fields.image.fields.file.url
-        return {title, latitude, longitude, address,id,category,image}
+        return { title, latitude, longitude, address, id, category, image }
       })
-       return markers
+      return markers
     } catch (error) {
       console.log(error)
     }
   }
 }
 
-
 //　Mapの初期化
 function initMap() {
   let myOptions = {
     zoom: 13,
-    center: new google.maps.LatLng(37.789096,-122.402170),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: new google.maps.LatLng(37.789096, -122.40217),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
   }
   let map = new google.maps.Map(
     document.getElementById("map_canvas"),
@@ -38,46 +44,46 @@ function initMap() {
   )
 
   const markers = new GetData()
-  markers.getLocation()
-  // jsonデータからマーカーの作成およびli要素の作成
-  .then( items => {
-    items.forEach( item => {
-      const id = item.id
-      const name = item.title
-      const latlng = new google.maps.LatLng(item.latitude, item.longitude)
-      const address = item.address
-      const image = item.image
+  markers
+    .getLocation()
+    // jsonデータからマーカーの作成およびli要素の作成
+    .then((items) => {
+      items.forEach((item) => {
+        const id = item.id
+        const name = item.title
+        const latlng = new google.maps.LatLng(item.latitude, item.longitude)
+        const address = item.address
+        const image = item.image
 
-      createMarker(name, latlng, map,id)
+        createMarker(name, latlng, map, id)
 
-      maplistUl.innerHTML += `<li><img src=${image} /><div> <h4>${name}</h4> Address : ${address}</div></li>`
+        maplistUl.innerHTML += `<li><img src=${image} /><div> <h4>${name}</h4> Address : ${address}</div></li>`
+      })
     })
-  })
-  // 生成されたli要素へのイベントリスナーの付与。
-  .then(()=>{
-    maplistLi = document.querySelectorAll('#mapList li')
-    maplistLi.forEach((listItem,index) => {
-      listItem.addEventListener('click',()=>{
-       google.maps.event.trigger(maplist[index], "click");
-     })
+    // 生成されたli要素へのイベントリスナーの付与。
+    .then(() => {
+      maplistLi = document.querySelectorAll("#mapList li")
+      maplistLi.forEach((listItem, index) => {
+        listItem.addEventListener("click", () => {
+          google.maps.event.trigger(maplist[index], "click")
+        })
+      })
     })
-  })
 }
 
 //　markerをクリックしたときの処理
-function createMarker(name, latlng, map,id) {
+function createMarker(name, latlng, map, id) {
   let infoWindow = new google.maps.InfoWindow()
   let marker = new google.maps.Marker({ position: latlng, map: map })
 
-  google.maps.event.addListener(marker, "click", function(e) {
-
+  google.maps.event.addListener(marker, "click", function (e) {
     // クリック済みのMakerに対応するliリストのCSS背景を初期化
-    maplistLi.forEach((item)=>{
-        if(item.classList.contains('clicked')){
-          console.log(item.textContent + "removed")
-          item.classList.remove('clicked')
-        }
-      })
+    maplistLi.forEach((item) => {
+      if (item.classList.contains("clicked")) {
+        console.log(item.textContent + "removed")
+        item.classList.remove("clicked")
+      }
+    })
 
     if (currentWindow) {
       currentWindow.close()
@@ -88,10 +94,9 @@ function createMarker(name, latlng, map,id) {
     map.panTo(latlng) //markerをクリックした時に地図の中心に
 
     //　クリックされたMarkerに対応するli要素のcss背景を操作する。
-    maplistLi[id-1].classList.add('clicked')
-
-    })
-    maplist[cnt++] = marker;
+    maplistLi[id - 1].classList.add("clicked")
+  })
+  maplist[cnt++] = marker
 }
 
 // ***** JQuery Sample *****
@@ -111,7 +116,4 @@ function createMarker(name, latlng, map,id) {
 //   })
 // });
 
-
 google.maps.event.addDomListener(window, "load", initMap)
-
-
