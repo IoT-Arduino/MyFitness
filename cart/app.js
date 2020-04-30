5c121af2185ab305edf2a17f9beb6a362513c025
unknown <bunkajin@icloud.com>
2020-04-30 16:21:31 +0900

    edit app.js change from class to arrow function

--- cart/app.js
+++ cart/app.js
@@ -1,37 +1,31 @@
 //variables
 const productsDOM = document.querySelector(".products-center")
-
-// cart
 let cart = []
-// buttons 
 let buttonsDOM = []
 
 // getting the products
-class Products {
-  async getProducts() {
-    try {
-      let result = await fetch("products.json")
-      let data = await result.json()
-      let products = data.items
-      products = products.map(item => {
-        const { title, price } = item.fields
-        const { id } = item.sys
-        const image = item.fields.image.fields.file.url
-        return {title,price,id,image}
-      })
-      return products
-    } catch (error) {
-      console.log(error)
-    }
+const getProducts = async () => {
+  try {
+    let result = await fetch("products.json")
+    let data = await result.json()
+    let products = data.items
+    products = products.map((item) => {
+      const { title, price } = item.fields
+      const { id } = item.sys
+      const image = item.fields.image.fields.file.url
+      return { title, price, id, image }
+    })
+    return products
+  } catch (error) {
+    console.log(error)
   }
 }
 
-// UI display class
-class UI {
-    displayProducts(products){
-        let result = '';
-        products.forEach(product => {
-            result  += `
+// UI display function
+const displayProducts = (products) => {
+  let result = ""
+  products.forEach((product) => {
+    result += `
             <!-- single product  -->
             <article class="product">
                 <div class="img-container">
@@ -65,20 +59,12 @@ class UI {
             </article>
             <!-- end of single product  -->
             `
-        })
-        productsDOM.innerHTML = result;
-    }
-
-
+  })
+  productsDOM.innerHTML = result
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ui = new UI()
-  const products = new Products()
-
-  // getl all products
-  products.getProducts().then(products => {
-     ui.displayProducts(products)
+  getProducts().then((products) => {
+    displayProducts(products)
   })
-
 })
